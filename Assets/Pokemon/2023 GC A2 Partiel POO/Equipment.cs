1feca5ce54101a982880610f5a2a8f3420f15bc4
Thidiane <lthidiane@gmail.com>
2025-01-10 17:37:55 +0100

    POO

---
+++ Assets/Pokemon/2023 GC A2 Partiel POO/Equipment.cs
@@ -0,0 +1,25 @@
+﻿
+namespace _2023_GC_A2_Partiel_POO.Level_2
+{
+    /// <summary>
+    /// Défintion simple d'un équipement apportant des boost de stats
+    /// </summary>
+    public class Equipment
+    {
+
+
+        public Equipment(int bonusHealth, int bonusAttack, int bonusDefense, int bonusSpeed)
+        {
+            BonusHealth = bonusHealth;
+            BonusAttack = bonusAttack;
+            BonusDefense = bonusDefense;
+            BonusSpeed = bonusSpeed;
+        }
+
+        public int BonusHealth { get; protected set; } = 100;
+        public int BonusAttack { get; protected set; } = 90;
+        public int BonusDefense { get; protected set; } = 70;
+        public int BonusSpeed { get; protected set; } = 12;
+
+    }
+}
